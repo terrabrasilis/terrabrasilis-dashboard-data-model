843136fbeabce287bcc62b94cd965662a529d9a7
Luiz Fernando Ferreira Gomes de Assis <luizffga@dpi.inpe.br>
2019-04-01 18:30:53 -0300

    initialize private schema

---
+++ initialize-private-data-model-schema.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# pass username, password, host and database name as parameter
+echo "Initializing private data model schema..."
+
+user="$1"
+password="$2"
+host="$3"
+port="$4"
+database="$5"
+
+psql postgresql://$user:$password@$host:$port/$database << EOF
+	\timing
+    
+    /* create raw data schema */
+	DROP SCHEMA private CASCADE;
+
+	CREATE SCHEMA private;
+
+	GRANT ALL ON SCHEMA private TO postgres;
+EOF
+	
