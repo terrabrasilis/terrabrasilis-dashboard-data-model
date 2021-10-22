8327f0082dbe251fd9643d19b7f04c0cc1820a44
André Carvalho <afacarvalho@yahoo.com.br>
2021-10-22 10:01:52 -0300

    Change filter condition to update areas

--- update_area.sh
+++ update_area.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Query="UPDATE features f SET area_km2 = ST_Area(ST_Transform(f.geom, 4326)::geography)/1000000
-WHERE f.created_at::date>=now()::date;"
+WHERE area_km2 IS NULL;"
 echo $Query
 RESPONSE_QUERY=$(./exec_query.sh $user $host $port $database "$Query")
 echo "PSQL Return: $RESPONSE_QUERY"
