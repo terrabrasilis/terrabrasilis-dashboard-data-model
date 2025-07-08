536ba5e43e015820c1558b6c2e5af433ee71a663
André Carvalho <afacarvalho@yahoo.com.br>
2025-07-08 09:20:16 -0300

    Disable and enable autovacuum to speed up the festure process.

--- features/run_all.sh
+++ features/run_all.sh
@@ -5,9 +5,21 @@ IFS=', ' read -r -a filter <<< "$processing_filter"
 # Meaning of lois IDs(1=uf, 2=mun, 3=conservation units, 4=indigenous areas) *without pathrow
 lois=(1 2 3 4)
 
+# ###############################
+echo "disable autovacuum to speed up the process"
+DISABLE_VACCUM="ALTER TABLE public.features SET (autovacuum_enabled = off);"
+psql -p $port -d $database -U $user -h $host -t -c "$DISABLE_VACCUM"
+# ###############################
+
 for data_features in "${filter[@]}"; do
     echo "Processing features from $data_features" > "output_${data_features}.log"
     for loi in "${lois[@]}"; do
         . ./$data_features.sh "$loi"
     done
 done
+
+# ###############################
+echo "re-enable autovacuum"
+ENABLE_VACCUM="ALTER TABLE public.features SET (autovacuum_enabled = on);"
+psql -p $port -d $database -U $user -h $host -t -c "$ENABLE_VACCUM"
+# ###############################
